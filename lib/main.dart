21b1beb27a9555550ea07f78dce5d1a47525d937
Hammad Khan <hammadkhn30@gmail.com>
2020-04-09 18:09:44 +0530

    question.dart commited

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,61 @@
 import 'package:flutter/material.dart';
-class MyApp extends StatelessWidget{
-  build(BuildContext context){
 
+import './question.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var question = [
+      'What\'s your favorite color?',
+      'Whats\'s your favorite animal?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Question(question[_questionIndex]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 4'),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
